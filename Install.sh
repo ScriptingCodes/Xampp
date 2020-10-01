330cb891159209a127f8cf7c7cb16c3d73f471f3
Senali Madawala <senalimadawala@gmail.com>
2020-10-01 14:19:23 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@
 
 #get xampp
 
-wget https://liquidtelecom.dl.sourceforge.net/project/xampp/XAMPP%20Linux/7.4.10/xampp-linux-x64-7.4.10-0-installer.run
+wget https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/7.4.10/xampp-linux-x64-7.4.10-0-installer.run?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fxampp%2Ffiles%2Flatest%2Fdownload&ts=1601540155
 
 #make it executable
 
