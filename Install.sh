40a5c91e0d658f94a99ec53f0eadb0532fdad5f7
Senali Madawala <senalimadawala@gmail.com>
2020-10-01 13:52:04 +0530

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#get xampp
+
+wget https://liquidtelecom.dl.sourceforge.net/project/xampp/XAMPP%20Linux/7.4.10/xampp-linux-x64-7.4.10-0-installer.run
+
+#make it executable
+
+sudo chmod +x xampp-linux-x64-7.4.10-0-installer.run
+
+#execute it
+
+./xampp-linux-x64-7.4.10-0-installer.run
